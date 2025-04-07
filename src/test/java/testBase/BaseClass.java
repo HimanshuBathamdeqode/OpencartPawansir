f04d0eb05216bb053c69d79048739f4910e331bd
deq <himanshu.1630@gmail.com>
2025-04-08 01:35:22 +0530

    refactor4

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -39,7 +39,7 @@ public Properties p;
 	@Parameters({"os","browser"})
 	public void setup(String os, String br) throws IOException
 	{
-		//Loading config.properties fil
+		//Loading config.properties
 		FileReader file=new FileReader("./src//test//resources//config.properties");
 		p=new Properties();
 		p.load(file);
